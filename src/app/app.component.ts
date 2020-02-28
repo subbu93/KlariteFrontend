85d7ad598166bd6bf57b30eb72cf4b77685eeaa1
subbu93 <subh.subhajit10@gmail.com>
2020-02-28 15:12:38 -0500

    - First commit of the front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'KlariteFrontend';
+export class AppComponent implements OnInit, OnDestroy {
+  public title: string;
+  navbarOpen = false;
+  navigationSubscription;
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+  constructor(private router: Router) {
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initialize the component
+      if (e instanceof NavigationEnd && e.url === '/home') {
+        this.initialiseInvites();
+      }
+    });
+  }
+  initialiseInvites() {
+    this.title = 'DASHBOARD';
+  }
+
+  ngOnInit(): void {
+    this.title = 'DASHBOARD';
+  }
+
+  ngOnDestroy(): void {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminSkillComponent } from './admin/skill/home/admin-skill.component';
+import { AdminSkillAddComponent } from './admin/skill/add/admin-skill-add.component';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { AdminTrainingComponent } from './admin/training/home/admin-training.component';
+import { AdminTrainingAddComponent } from './admin/training/add/admin-training-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminSkillComponent,
+    AdminSkillAddComponent,
+    AdminTrainingComponent,
+    AdminTrainingAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
